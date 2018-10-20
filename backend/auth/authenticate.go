cb05f31048f6e55f7839c6abb940be8d7769e77b
sea350 <sea350@nyu.edu>
2018-10-20 11:03:13 -0400

    fixed syntax errors

--- backend/auth/authenticate.go
+++ backend/auth/authenticate.go
@@ -12,7 +12,7 @@ import (
 )
 
 //Authenticate validates an authentication request, works with pb req
-func (eclient *Eclient) Authenticate(ctx context.Context, req *authpb.AuthenticateRequest) (*authpb.AuthenticationResponce, error) {
+func (eclient *Eclient) Authenticate(ctx context.Context, req *authpb.AuthenticateRequest) (*authpb.AuthenticateResponse, error) {
 
 	//pull soted data attached to the email
 	query := elastic.NewTermQuery("Email", strings.ToLower(req.Email))
@@ -22,31 +22,31 @@ func (eclient *Eclient) Authenticate(ctx context.Context, req *authpb.Authentica
 		Do(ctx)
 
 	if err != nil {
-		return &authpb.AuthenticationResponce{}, err
+		return &authpb.AuthenticateResponse{}, err
 	}
 
 	//there should never be more than one result. If there is, there is an issue
 	if res.Hits.TotalHits > 1 {
-		return &authpb.AuthenticationResponce{}, errors.New("More than one result")
+		return &authpb.AuthenticateResponse{}, errors.New("More than one result")
 	}
 
 	var usr authpb.Stored
-	var ID string //this is left here in case it is needed in the future
+	//var ID string //this is  here in case it is needed in the future
 
 	for _, element := range res.Hits.Hits {
-		ID = element.Id
+		//ID = element.Id
 		err := json.Unmarshal(*element.Source, &usr)
 		if err != nil {
-			return &authpb.AuthenticationResponce{}, err
+			return &authpb.AuthenticateResponse{}, err
 		}
 		break
 	}
 
 	//validate the password
-	valid := bcrypt.CompareHashAndPassword(usr.Password, req.Challenge)
-	if !valid {
-		return &authpb.AuthenticationResponce{}, errors.New("Invalid password")
+	err = bcrypt.CompareHashAndPassword([]byte(usr.Password), []byte(req.Challenge))
+	if err != nil {
+		return &authpb.AuthenticateResponse{}, err
 	}
 
-	return &authpb.AuthenticationResponce{Success: true}, nil
+	return &authpb.AuthenticateResponse{Success: true}, nil
 }
