8f7d56806ea1a562f6bb84fb48162c6baa743ec8
Adam Sanghera <adamsanghera@gmail.com>
2018-10-23 13:25:20 -0400

    example config in backend.go

--- backend/backend.go
+++ backend/backend.go
@@ -4,26 +4,29 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/sea350/ustart_mono/backend/api/rest/auth"
+	authapi "github.com/sea350/ustart_mono/backend/api/rest/auth"
+	auth "github.com/sea350/ustart_mono/backend/auth"
 )
 
 // Server is a monolithic service providing access to all of UStart's data
 type Server struct {
 	port    string
-	authSrv *auth.ElasticAuthRESTAPI
+	authAPI *authapi.ElasticAuthRESTAPI
 }
 
 // New returns a new backend server, given the config object
 func New(cfg *Config) (*Server, error) {
 	// creates all api servers
 
-	authSrv, err := auth.New(&auth.Config{})
+	authAPI, err := authapi.New(&authapi.Config{
+		EAuthCfg: auth.NewConfig(),
+	})
 	if err != nil {
 		return nil, err
 	}
 
 	return &Server{
-		authSrv: authSrv,
+		authAPI: authAPI,
 	}, err
 }
 
@@ -32,7 +35,7 @@ func (srv *Server) Run() error {
 	log.SetPrefix("Backend Server:")
 	log.Println("Booting...")
 
-	http.HandleFunc("/auth/register", srv.authSrv.Register)
+	http.HandleFunc("/auth/register", srv.authAPI.Register)
 
 	log.Printf("Listening on %s\n", srv.port)
 	return http.ListenAndServe(":"+srv.port, nil)
