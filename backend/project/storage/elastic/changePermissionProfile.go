ae437291f73d1746bd029bdf3752413334883594
Jonchow77 <Jonchow77@gmail.com>
2019-01-04 16:54:32 -0500

    Added New Errors, Edited Change Permission Profile

--- backend/project/storage/elastic/changePermissionProfile.go
+++ backend/project/storage/elastic/changePermissionProfile.go
@@ -6,11 +6,15 @@ import (
 	"github.com/olivere/elastic"
 )
 
+//------------------------------------------WIP---------------------------------------------
+
 //ChangePermissionProfile Changes a user's permission profile
-func (estor *ElasticStore) ChangePermissionProfile(ctx context.Context, userID string, targerUserID string, projectID string) error {
+func (estor *ElasticStore) ChangePermissionProfile(ctx context.Context, userID string, targetUserID string, projectID string) error {
 	//Make sure user changing permission profile has correct permissions
 	// query := elastic.NewTermQuery("UUID", userID)
-	query := elastic.NewTermsQuery()
+	query := elastic.NewBoolQuery()
+	query = query.Must(elastic.NewTermQuery("UUID", userID))
+	query = query.Must(elastic.NewTermQuery("PID", projectID))
 	res, err := estor.client.Search().
 		Index(estor.eIndex).
 		Query(query).
@@ -20,7 +24,39 @@ func (estor *ElasticStore) ChangePermissionProfile(ctx context.Context, userID s
 		return err
 	}
 
+	//If no results, current user does not have permission to change permission or user not found
+	if res.Hits.TotalHits < 1 {
+		return ErrUserDoesNotExist
+	}
+
+	//If too many results, critical error. Should not be more than 1 result
+	if res.Hits.TotalHits > 1 {
+		return ErrTooManyResults
+	}
+
 	//Pull target user from database
+	userQuery := elastic.NewBoolQuery()
+	userQuery = userQuery.Must(elastic.NewTermQuery("UUID", targetUserID))
+
+	userRes, err := estor.client.Search().
+		Index(estor.eIndex).
+		Query(query).
+		Do(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	//If no results, target user could not be found
+	if userRes.Hits.TotalHits < 1 {
+		return ErrUserDoesNotExist
+	}
+
+	//If too many results, critical error. Should not be more than 1 result
+	if userRes.Hits.TotalHits > 1 {
+		return ErrTooManyResults
+	}
+
 	//Update permission profile of targer user
 
 }
--- backend/project/storage/elastic/errors.go
+++ backend/project/storage/elastic/errors.go
@@ -1,14 +1,16 @@
 package elasticstore
 
+import "errors"
+
 var (
-// // ErrUserDoesNotExist user doesnt exist
-// ErrUserDoesNotExist = errors.New("User does not exist")
+	// ErrUserDoesNotExist user doesnt exist
+	ErrUserDoesNotExist = errors.New("User does not exist")
 
-// // ErrTooManyResults if more than one result per email shows
-// ErrTooManyResults = errors.New("Too many results, a crititcal error has occurred")
+	// ErrTooManyResults if more than one result per email shows
+	ErrTooManyResults = errors.New("Too many results, a crititcal error has occurred")
 
-// // ErrEmailInUse happens when a signup using an existing email occurs
-// ErrEmailInUse = errors.New("Email in use")
+	// ErrEmailInUse happens when a signup using an existing email occurs
+	ErrEmailInUse = errors.New("Email in use")
 
 //
 )
