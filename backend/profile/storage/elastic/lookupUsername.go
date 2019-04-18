252757b2d9b9201b912704b70ca058d45c38e2b8
NEVETS <sea350@nyu.edu>
2019-04-18 16:49:03 -0400

    added more storage functions

---
+++ backend/profile/storage/elastic/lookupUsername.go
@@ -0,0 +1,43 @@
+package elasticstore
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/sea350/ustart_micro/backend/profile/profilepb"
+
+	"github.com/olivere/elastic"
+)
+
+// LookupUsername pulls up if a document exists using a certain username
+func (estor *ElasticStore) LookupUsername(ctx context.Context, username string) (profilepb.Profile, error) {
+	var profile profilepb.Profile
+
+	termQuery := elastic.NewTermQuery("Username", username)
+	res, err := estor.client.Search().
+		Index(estor.eIndex).
+		Query(termQuery).
+		Do(ctx)
+
+	if err != nil {
+		return profile, err
+	}
+
+	// if there are no hits, then no one exists by that username
+	if res.Hits.TotalHits < 1 {
+		return profile, ErrUserDoesNotExist
+	}
+
+	// if theres more than a single result then a problem has occurred
+	if res.Hits.TotalHits > 1 {
+		return profile, ErrTooManyResults
+	}
+
+	for _, elem := range res.Hits.Hits {
+
+		err := json.Unmarshal(*elem.Source, &profile)
+		return profile, err
+	}
+
+	return profile, nil
+}
--- backend/profile/storage/storage.go
+++ backend/profile/storage/storage.go
@@ -2,12 +2,16 @@ package storage
 
 import (
 	"context"
+
+	"github.com/sea350/ustart_micro/backend/profile/profilepb"
 )
 
 // Storage is a database-agnostic interface for persisting profile data
 type Storage interface {
 	Register(context.Context, string, string, string, string, string, string, string, bool, bool) error
 	Lookup(context.Context, string) (string, error)
+	Search(context.Context, []string, bool, bool, bool, map[string][]string, string) ([]string, error)
+	LookupUsername(context.Context, string) (profilepb.Profile, error)
 
 	// rest of the functions
 	ErrUserDoesNotExist() error
