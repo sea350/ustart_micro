fa3eda72fd940f379108cac5a72bd0321ea672f5
th1750@nyu.edu <th1750@nyu.edu>
2019-05-15 14:19:46 -0400

    New

---
+++ backend/showcase/widget/storage/elastic/deleteWidget.go
@@ -0,0 +1,10 @@
+package elastic
+
+import (
+	"context"
+	//"github.com/sea350/ustart_mono/backend/widget/wigetspb" --> Doesn't want to implement???
+)
+
+DeleteWidget(context.Context, wdigetID string) error{
+	//Unsure how to implement, ask next week
+}
