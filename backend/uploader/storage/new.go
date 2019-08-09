5c7339ab03255a8518896141dfcbb8f22c05a6cb
J0NL1N <jl8918@nyu.edu>
2019-08-09 11:46:16 -0400

    SQL storage and new update

--- backend/uploader/storage/new.go
+++ backend/uploader/storage/new.go
@@ -6,6 +6,7 @@ import (
 
 // NewSQL determines the runtime behavior of the ElasticSearch-backed customer server
 func NewSQL(config *Config) (Storage, error) {
-	strg, err := &Config{SQLConfig: sqlstore.NewConfig()}
+//	strg, err := &Config{SQLConfig: sqlstore.NewConfig()}
+	strg, err := sqlstore.New(config.SQLConfig)
 	return strg, err
 }
--- backend/uploader/storage/storage.go
+++ backend/uploader/storage/storage.go
@@ -1,13 +1,18 @@
 package storage
 
+import (
+	"context"
+)
 // type Storage interface {
 // 	Upload(context.Context, string, string) error
 // 	Delete(context.Context, string, string) error
 // 	//Err____ to handle errors
 //Storage holds the necessary functions
 type Storage interface {
-	UploadBanner()
-	DeleteBanner()
-	UploadProfile()
-	DeleteProfile()
+	UploadBanner(conext.Context, string, string)error
+	DeleteBanner(context.Context, string, string)error
+	UploadProfile(context.Context, string, string)error
+	DeleteProfile(context.context, string, string)error
+
+	ErrImproperImport() error
 }
