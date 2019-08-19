3079220c19b22d94b9c5ffe086cbcc907007c343
J0NL1N <jl8918@nyu.edu>
2019-08-19 16:02:18 -0400

    minor uploader sql work

--- backend/uploader/storage/storage.go
+++ backend/uploader/storage/storage.go
@@ -9,9 +9,9 @@ import (
 // 	//Err____ to handle errors
 //Storage holds the necessary functions
 type Storage interface {
-	UploadBanner(conext.Context, string, string)error
+	UploadBanner(conext.Context, string, string, string)error
 	DeleteBanner(context.Context, string, string)error
-	UploadProfile(context.Context, string, string)error
+	UploadProfile(context.Context, string, string, string)error
 	DeleteProfile(context.context, string, string)error
 
 	ErrImproperImport() error
