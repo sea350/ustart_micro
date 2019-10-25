e7c6eb36ac1617a2e8aa8119ea79535d20ea001b
Mithila Das <md3756@nyu.edu>
2019-10-25 16:21:23 -0400

    made edits to service.proto and storage.go

--- backend/uploader/storage/storage.go
+++ backend/uploader/storage/storage.go
@@ -3,14 +3,13 @@ package storage
 import (
 	"context"
 )
+
 // type Storage interface {
 // 	Upload(context.Context, string, string) error
 // 	Delete(context.Context, string, string) error
 // 	//Err____ to handle errors
 //Storage holds the necessary functions
 type Storage interface {
-	Upload(conext.Context, string, string) (string,error)
-	Delete(context.Context, string, string)error
-
-	ErrImproperImport() error
+	Upload(conext.Context, string, string) (string, error)
+	Delete(context.Context, string, string) error
 }
