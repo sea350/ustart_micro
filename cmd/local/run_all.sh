16135f0e64416b84a3ece8a6dee8873d5bd8d240
sea350 <sea350@nyu.edu>
2020-02-18 16:34:27 -0500

    adjusting deployment

--- cmd/local/run_all.sh
+++ cmd/local/run_all.sh
@@ -8,15 +8,15 @@ export USTART_PROJ_PORT="5003"
 export BACKEND_PORT="5000"
 
 
-# Then, we build the services
-go build -o backend backend.go
-go build -o microservices_grpc/auth/auth microservices_grpc/auth/auth.go
-go build -o microservices_grpc/profile/prof microservices_grpc/profile/profile.go
-go build -o microservices_grpc/project/proj microservices_grpc/project/project.go
-
+# Then, we run the services
+# you should build and run binaries when in production
+go run -o microservices_grpc/auth/auth microservices_grpc/auth/auth.go
+go run -o microservices_grpc/profile/prof microservices_grpc/profile/profile.go
+go run -o microservices_grpc/project/proj microservices_grpc/project/project.go
+go run -o backend backend.go &>backend_log.txt &disown
 
 # Next, we execute them
-./cmd/backend/backend &>backend_log.txt &disown
+# ./cmd/backend/backend &>backend_log.txt &disown
 
 echo $?
 
