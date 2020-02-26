ef31c4dabfc59f8e70bcc568d2f35ec55c4a5c9e
sea350 <sea350@nyu.edu>
2020-02-26 11:03:50 -0500

    :)

---
+++ cmd/local/README.md
@@ -0,0 +1,24 @@
+# HOW TO RUN
+
+Create a `config.json` file. Then create a session config object, this will be imported into the run and unmarshalled. 
+
+## Sample Config object
+
+You can verbatim copy paste this object into your `config.json` but remember to fill out proper credentials depending on your run environment.
+
+```json
+{{
+    "StorageConfig":{
+        "SQLConfig":{
+            "DriverName":"postgres",
+            "Host":"localhost",
+            "Port":"5432",
+            "DBName":"test",
+            "Username":"postgres",
+            "Password":"password",
+            "SessionTableName":"sess"
+        }
+    },
+    "SessionKey":"session_please"
+}
+```
--- cmd/local/backend.go
+++ cmd/local/backend.go
@@ -1,37 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"os"
 
 	"github.com/sea350/ustart_micro/backend"
 	"github.com/sea350/ustart_micro/backend/session"
-	"github.com/sea350/ustart_micro/backend/session/storage"
-	sqlstore "github.com/sea350/ustart_micro/backend/session/storage/sql"
 )
 
 func main() {
 	log.SetPrefix("Backend Command, ")
 	log.Println("Loading config...")
+
+	var config session.Config
+	//Importing configuration from json
+	file, err := os.Open("config.json")
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.NewDecoder(file).Decode(&config)
+	if err != nil {
+		panic(err)
+	}
+
 	cfg := &backend.Config{
 		Port:               os.Getenv("BACKEND_PORT"),
 		AuthAPIAdress:      "http://localhost:" + os.Getenv("USTART_AUTH_PORT"),
 		ProfileAPIAdresss:  "http://localhost:" + os.Getenv("USTART_PROF_PORT"),
 		ProjectAPIAddress:  "http://localhost:" + os.Getenv("USTART_PROJ_PORT"),
 		UploaderAPIAddress: "http://localhost:" + os.Getenv("USTART_UPLOADER_PORT"),
-		SessionConfig: session.Config{
-			StorageConfig: &storage.Config{
-				SQLConfig: &sqlstore.Config{
-					DriverName: "postgres",
-					Host:       "ustart-postgres-test.crgo1xxhh8o5.us-east-1.rds.amazonaws.com",
-					Port:       "5432",
-					DBName:     "ustart-postgres-test",
-					Username:   "ustart",
-					Password:   "imadirtyweeb",
-				},
-			},
-			SessionKey: "session_please",
-		},
+		SessionConfig:      config,
 	}
 
 	log.Println("Creating new backend service from config...")
