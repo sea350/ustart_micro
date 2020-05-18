279be060006307ce8f370522d3ae15bdf770ae98
nevetsu <sea350@nyu.edu>
2020-05-18 11:47:09 -0400

    apt install added

---
+++ install_apt.sh
@@ -0,0 +1,20 @@
+apt install git golang tree -y
+
+export GO111MODULE=on
+go get google.golang.org/grpc@v1.28.1
+
+PROTOC_VERSION=3.11.4# change as needed
+PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+curl -LO $PB_REL/download/v$PROTOC_VERSION/protoc-$PROTOC_VERSION-linux-x86_64.zip
+unzip protoc-$PROTOC_VERSION-linux-x86_64.zip -d $HOME/.local
+export PATH="$PATH:$HOME/.local/bin"
+rm -f protoc-$PROTOC_VERSION-linux-x86_64.zip
+
+
+export GOPATH=$HOME/go # change as needed
+go get github.com/golang/protobuf/protoc-gen-go
+export PATH="$PATH:$(go env GOPATH)/bin"
+export PATH=$PATH:/usr/local/go/bin
+
+go get github.com/olivere/elastic
+env GIT_TERMINAL_PROMPT=1 go get github.com/sea350/ustart_micro
